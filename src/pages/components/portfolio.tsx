c798844fe8985eb74d24b1102d8c481128f7bca4
Kris <kris@kaperi.com>
2023-06-29 11:08:42 +0200

    working on frontpage, show description

--- src/pages/components/portfolio.tsx
+++ src/pages/components/portfolio.tsx
@@ -14,7 +14,7 @@ function KaperiPortfolio() {
     return (
         <div className='text-slate-300 flex flex-wrap w-[90%] justify-center items-center'>
             {listOfPages.map((event, index) => (
-                <div className='relative bg-purple-700 hover:bg-cyan-600 hover:font-semibold rounded-xl text-center m-3 h-20 w-[25%] border-black border shadow-lg flex justify-center items-center' key={index}>
+                <div className='relative bg-purple-700 hover:bg-cyan-600 rounded-t-xl text-center m-3 mb-14 h-20 w-[25%] max-w-[10rem] text-clamp border-black border shadow-lg flex basis-full justify-center items-center' key={index}>
                     <div className='absolute right-1 -top-3'>
                         {event.type === "Component" ?
                             <IcT icons="components" classNameCustom="h-[1.2rem] w-[1.2rem]" />
@@ -25,7 +25,12 @@ function KaperiPortfolio() {
                     <Link href={event.type === "Component" ? `/portfolio/components/${event.fileName.slice(0,-4)}` :  `/portfolio/templates/${event.fileName.slice(0,-4)}`} key={index}>
                         {event.title}
                     </Link>
+
+                    <div className='h-14 w-40 rounded-b-xl bg-slate-300 border-black absolute border -bottom-14 text-black overflow-hidden'>
+                        {event.desc}
+                    </div>
                 </div>
+
             ))}
         </div>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        clamp: "clamp(0.8rem, 2vw, 1.05rem)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
