7d5e6286301eec0701eff0972978ba1c296bdb52
captainkapnap <65526453+captainkapnap@users.noreply.github.com>
2023-08-07 17:18:18 +0800

    input box done

--- src/pages/portfolio/components/input-boxes.tsx
+++ src/pages/portfolio/components/input-boxes.tsx
@@ -1,11 +1,21 @@
 import IcT from "~/pages/components/icons";
+import { useEffect, useRef, useState } from "react";
+import type { RefObject } from "react";
 
 function ICodeThis() {
     // ================ STATE ================
-    
+    const inputSingleRef = useRef<HTMLInputElement | null>(null);
+    const inputMultiRef = useRef<RefObject<HTMLButtonElement>[]>([]);
+    const [inputEmpty, setInputEmpty] = useState<boolean>(true);
     
     // ================ HELPERS ================
-
+    function handleInputChange() {
+        if (inputSingleRef.current && inputSingleRef.current.value.length > 0 && inputEmpty === true) {
+            setInputEmpty(false);
+        } else if (inputSingleRef.current && inputSingleRef.current.value.length === 0 && inputEmpty === false) {
+            setInputEmpty(true);
+        }
+    }
 
     // ================ LIFECYCLE ================
 
@@ -15,14 +25,17 @@ function ICodeThis() {
       <div id="toggleDarkDiv" className="dark">
         <div id="bodyDiv" className="min-h-screen bg-slate-300 flex justify-center items-center">
             <div className='input-label-move__wrapper | shadow-lg'>
-                
+
                 <input 
                     type="text"
                     id="input-label-move"
                     name="input-label-move"
                     placeholder="Enter some text"
                     required
-                    className='input-label-move__field input_remove_formatting'
+                    className={` ${inputEmpty === false ? 'input-label-move__notEmpty' : '' } input-label-move__field input_remove_formatting`}
+                    ref={inputSingleRef} // could set the translates up by setting height of inputbox to css property..
+                    onChange={handleInputChange}
+
                     // title="this is a title"  // tooltip here
                 />
                 <label
